ae9fa0d13bb1717b9ac60d4a51ffb65be3b3ac28
Casey <caseylewis54@yahoo.com>
2022-10-01 19:44:09 -0500

    Updating date_helper.py related tests and functions.

--- DataLib/date_helper.py
+++ DataLib/date_helper.py
@@ -1,13 +1,60 @@
 import datetime
 
+_MONTHS_IN_YEAR = 12
 
-def get_date_plus_one_year(date):
+
+def get_date_increment_year(date, years=1):
+    """
+    Allows you to get a datetime.datetime object of a different year, or the closest thing to it.
+
+    Handles leap years (February 29th) by decrementing by one day, so that it will remain as the last day of February.
+    :param date: datetime.datetime object
+    :param years: Number of years to increment OR decrement (decrement by passing negative integer)
+    :return: datetime.datetime object
+    """
     month = date.month
     day = date.day
-    year = date.year + 1
+    year = date.year + years
     try:
         next_year = datetime.datetime(year, month, day)
     except ValueError:
         next_year = datetime.datetime(year, month, day-1)
 
     return next_year
+
+
+def get_date_plus_one_month(date, months=1):
+    """
+    Allows you to get a datetime.datetime object of a different month, or the closest thing to it.
+
+    Handles passing over a year (December 31st) by setting month to January and incrementing the year.
+    For example December 31st, 2022 + 1 year ends up as January 31st, 2023.
+
+    Values greater than 11 or less than -11 not valid as the get_date_increment_year() function should be used instead.
+    :param date: datetime.datetime object
+    :param months: Number of months to increment OR decrement (decrement by passing negative integer)
+    :return:datetime.datetime object
+    """
+    if -(_MONTHS_IN_YEAR - 1) > months or months > _MONTHS_IN_YEAR - 1:
+        exc_text = f"Invalid month: [{months}]. " \
+                   f"Please use get_date_increment_year() function if incrementing months outside of -12 to 11."
+        raise MonthOutOfRange(exc_text)
+
+    month = date.month + months
+    day = date.day
+    year = date.year
+    if month > _MONTHS_IN_YEAR:
+        month = month % _MONTHS_IN_YEAR
+        year += 1
+    elif month < 1:
+        month += _MONTHS_IN_YEAR
+        year -= 1
+    try:
+        next_month = datetime.datetime(year, month, day)
+    except ValueError:
+        next_month = datetime.datetime(year, month, day-1)
+    return next_month
+
+
+class MonthOutOfRange(Exception):
+    pass
--- DataLib/tests/test_date_helper.py
+++ DataLib/tests/test_date_helper.py
@@ -9,18 +9,65 @@ except:
 class TestDates(unittest.TestCase):
 
     def test_get_date_plus_one_year(self):
+        # SINGLE YEAR #############################
         # LEAP DAY/YEAR
         leap_day = datetime.datetime(2024, 2, 29)
-        next_year = get_date_plus_one_year(leap_day)
+        next_year = get_date_increment_year(leap_day)
         self.assertEqual(next_year, datetime.datetime(2025, 2, 28))
         # FIRST OF MONTH
         first_of_month = datetime.datetime(2022, 10, 1)
-        next_year = get_date_plus_one_year(first_of_month)
+        next_year = get_date_increment_year(first_of_month)
         self.assertEqual(next_year, datetime.datetime(2023, 10, 1))
         # LAST OF MONTH
         last_of_month = datetime.datetime(2022, 12, 31)
-        next_year = get_date_plus_one_year(last_of_month)
+        next_year = get_date_increment_year(last_of_month)
         self.assertEqual(next_year, datetime.datetime(2023, 12, 31))
+
+        # MULTIPLE YEARS ##############################
+        # LEAP DAY/YEAR + 4 years
+        leap_day = datetime.datetime(2024, 2, 29)
+        next_year = get_date_increment_year(leap_day, years=4)
+        self.assertEqual(next_year, datetime.datetime(2028, 2, 29))
+        # LEAP DAY/YEAR - 4 years
+        leap_day = datetime.datetime(2024, 2, 29)
+        next_year = get_date_increment_year(leap_day, years=-4)
+        self.assertEqual(next_year, datetime.datetime(2020, 2, 29))
+
+    def test_get_date_plus_one_month(self):
+        # OUT OF BOUNDS ###################################
+        first_of_year = datetime.datetime(2022, 1, 1)
+        with self.assertRaises(MonthOutOfRange):
+            get_date_plus_one_month(first_of_year, months=13)
+        with self.assertRaises(MonthOutOfRange):
+            get_date_plus_one_month(first_of_year, months=-12)
+        # ADDING SINGLE MONTH #############################
+        # March 31st
+        march_end = datetime.datetime(2022, 3, 31)
+        next_month = get_date_plus_one_month(march_end)
+        self.assertEqual(next_month, datetime.datetime(2022, 4, 30))
+        # DECEMBER 31st
+        year_end = datetime.datetime(2022, 12, 31)
+        next_month = get_date_plus_one_month(year_end)
+        self.assertEqual(next_month, datetime.datetime(2023, 1, 31))
+
+        # ADDING MULTIPLE MONTHS #############################
+        # March 31st - July 31st
+        march_end = datetime.datetime(2022, 3, 31)
+        next_month = get_date_plus_one_month(march_end, months=4)
+        self.assertEqual(next_month, datetime.datetime(2022, 7, 31))
+        # March 31st - June 30th
+        march_end = datetime.datetime(2022, 3, 31)
+        next_month = get_date_plus_one_month(march_end, months=3)
+        self.assertEqual(next_month, datetime.datetime(2022, 6, 30))
+
+        # SUBTRACTING MONTHS #############################
+        january_start = datetime.datetime(2022, 1, 1)
+        previous_month = get_date_plus_one_month(january_start, months=-1)
+        self.assertEqual(previous_month, datetime.datetime(2021, 12, 1))
+        # January 31st - 2 months = November 30th
+        january_start = datetime.datetime(2022, 1, 31)
+        previous_month = get_date_plus_one_month(january_start, months=-2)
+        self.assertEqual(previous_month, datetime.datetime(2021, 11, 30))
 
 
 
